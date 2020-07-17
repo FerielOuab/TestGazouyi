caaeba6578778bbf0769fc2fbbfd5a6e1ea90c94
Feriel <feriel.ouab@gmail.com>
2020-07-17 13:59:00 +0200

    commit final: create account bug résolu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {CreateAccountComponent} from './create-account/create-account.component'
+import {MyAccountComponent} from './my-account/my-account.component';
+import {FormsModule} from '@angular/forms';
+import { Camera } from '@ionic-native/Camera/ngx';
+import {WebView} from '@ionic-native/ionic-webview/ngx';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CreateAccountComponent, MyAccountComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    WebView,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
