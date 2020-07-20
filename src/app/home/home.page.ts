70d8b1b22594fb1b6aedc44dbf5e9f17e978b45b
FerielOuab <feriel.ouab@gmail.com>
2020-07-20 02:11:18 +0200

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {LoginService} from '../services/login.service';
 import {Router} from "@angular/router";
+import {NavController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -12,20 +14,35 @@ export class HomePage {
   username: string;
   password: string;
   champObligatoire = true;
+  compteExists = true;
   constructor(private loginService: LoginService,
-              private router: Router) {}
+              private router: Router,
+              private navCtrl: NavController) {}
 
+  update(){
+      this.compteExists=true;
+  }
   accountExists(){
     console.log("LISTE USERS taille: " + this.loginService.map.size)
-    if(this.password!=null && this.username!=null)
+    if(this.password != null && this.username != null)
     {
       if(this.loginService.map.get(this.username) == this.password)
       {
+        this.compteExists = true;
         console.log("Connexion réussie !")
         this.router.navigate(['/home/MyAccount']);
       }
+      else
+      {
+
+        this.compteExists = false;
+
+
+
+      }
     }
     else{
+
       this.champObligatoire = false;
     }
   }
